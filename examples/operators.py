5948eddb498ec3cb3eb6de4ad8c32e0f9ad86940
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-03-31 08:43:09 +0200

    added operators

---
+++ examples/operators.py
@@ -0,0 +1,68 @@
+
+a = 10
+b = 5
+c = a + b
+print(c)
+
+# arithmetic op.:
+# Addition: +
+# Subtraction: -
+# Multiplication: *
+# Division: /
+# Modulus: %
+# Exponentiation: **
+# Floor division: //
+
+# assignment op.:
+x = 5
+x = x + 3
+x += 3
+
+x = x ** 3
+x **= 3
+
+# comparison op.:
+# Equal: ==
+# Not equal: !=
+# Greater than: >
+# Less than: <
+# Greater than or equal to: >=
+# Less than or equal to: <=
+
+# logical o.:
+# and
+# or
+# not
+
+x = 5
+print(x > 3 and x < 10)
+print(3 < x < 10)
+
+# identiy op.:
+# is
+# is not
+
+a = 'Hello'
+b = 'HELLO'
+c = 'Hello'
+
+print(a is c)
+print(a is b)
+
+a = a.lower()
+print(a)
+b = b.lower()
+print(b)
+print(a is b)
+
+# membership op.:
+# in
+# not in
+a = 'Hello world'
+b = 'Hello'
+
+print(b in a)
+
+Europe = ['Italy', 'France', 'Germany', 'Portugal', 'Spain', 'Belgium', 'Slovenia']
+
+print ('Britain' in 'Eurpore')
