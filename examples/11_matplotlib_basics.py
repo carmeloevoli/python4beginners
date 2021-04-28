96abce10513ae7791099c6e6c7e293ad9701af43
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-04-28 07:49:32 +0200

    add links for matplotib

--- examples/11_matplotlib_basics.py
+++ examples/11_matplotlib_basics.py
@@ -4,6 +4,25 @@ matplotlib.use('MacOSX')
 
 import matplotlib.pyplot as plt
 
+import numpy as np
+from scipy import fftpack
+
+np.random.seed(1234)
+
+time_step = 0.02
+period = 5.
+
+time_vec = np.arange(0, 20, time_step)
+sig = (np.sin(2 * np.pi / period * time_vec)
+       + 0.5 * np.random.randn(time_vec.size))
+
+plt.figure(figsize=(6, 5))
+plt.plot(time_vec, sig, label='Original signal')
+
+https://scipy-lectures.org/intro/scipy/auto_examples/plot_fftpack.html
+
+https://matplotlib.org/stable/gallery/index.html
+
 #plt.plot([1,2,3,4],[1,4,9,16])
 #plt.title('first plot')
 #plt.xlabel('X')
