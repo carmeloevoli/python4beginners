730aa8cf931df00b6e76082a16295a31061b1deb
Carmelo Evoli <carmelo.evoli@gssi.it>
2021-04-28 11:42:14 +0200

    final matplotlib example

--- examples/11_matplotlib_basics.py
+++ examples/11_matplotlib_basics.py
@@ -1,36 +1,57 @@
 ### Lesson 11 - matplotlib basics
+### Gallery: https://matplotlib.org/stable/gallery/index.html
+
 import matplotlib
 matplotlib.use('MacOSX')
-
 import matplotlib.pyplot as plt
 
 import numpy as np
-from scipy import fftpack
 
 np.random.seed(1234)
 
+def read_signal(time_step, period):
+    time = np.arange(0, 20, time_step)
+    noise = 0.5 * np.random.randn(time.size)
+    sig = np.sin(2 * np.pi / period * time) + noise
+    return time, sig
+
+def filter_signal(time, sig):
+    from scipy import fftpack
+    sig_fft = fftpack.fft(sig)
+    power = np.abs(sig_fft)**2
+
+    sample_freq = fftpack.fftfreq(sig.size, d=time_step)
+
+    pos_mask = np.where(sample_freq > 0)
+    freqs = sample_freq[pos_mask]
+    peak_freq = freqs[power[pos_mask].argmax()]
+
+    high_freq_fft = sig_fft.copy()
+    high_freq_fft[np.abs(sample_freq) > peak_freq] = 0
+    filtered_sig = fftpack.ifft(high_freq_fft)
+    
+    return filtered_sig
+
+# THE PLOT
+
 time_step = 0.02
-period = 5.
+period = 5.0
 
-time_vec = np.arange(0, 20, time_step)
-sig = (np.sin(2 * np.pi / period * time_vec)
-       + 0.5 * np.random.randn(time_vec.size))
+time, signal = read_signal(time_step, period)
+filtered_signal = filter_signal(time, signal)
 
-plt.figure(figsize=(6, 5))
-plt.plot(time_vec, sig, label='Original signal')
+fig = plt.figure(figsize=(9, 7))
+ax = fig.add_subplot(111)
 
-https://scipy-lectures.org/intro/scipy/auto_examples/plot_fftpack.html
+ax.set_xlabel('Time [s]')
+ax.set_ylabel('Amplitude')
 
-https://matplotlib.org/stable/gallery/index.html
+ax.plot(time, signal, label='Original signal', linewidth=2, color='blue', linestyle='-')
+ax.plot(time, filtered_signal, label='Filtered signal', linewidth=3, color='red', linestyle=':')
 
-#plt.plot([1,2,3,4],[1,4,9,16])
-#plt.title('first plot')
-#plt.xlabel('X')
-#plt.ylabel('Y')
+ax.text(1, -2, 'FFT', fontsize=20)
+
+ax.legend(loc='best')
+
 #plt.show()
-
-#plt.figure(figsize=(15,5))
-#plt.plot([1,2,3,4],[1,4,9,16])
-#plt.show()
-
-fig, ax = plt.figure(figsize=([15,5]))
+plt.savefig('FFT.pdf')
